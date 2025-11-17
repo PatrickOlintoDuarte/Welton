40ad3e1e9838974c5121fac93bc9b2376e9eb849
Patrick <100382449+PatrickOlintoDuarte@users.noreply.github.com>
2025-11-17 17:04:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('keydown', function (event) {
 
 // ===== CONFIG E SESSÃO =====
 const config = {
-  N8N_WEBHOOK_URL: 'https://flow.grupopanda.net.br/webhook-test/elton',
+  N8N_WEBHOOK_URL: 'https://flow.grupopanda.net.br/webhook/elton',
   RETRY_ATTEMPTS: 3,
   RETRY_DELAY: 1000
 };
@@ -610,3 +610,4 @@ class ChatElton {
 // Inicializa
 const chat = new ChatElton();
 console.log('✅ Chat Elton Suporte TI carregado com suporte a Áudio e Anexos (compositor)!');
+
